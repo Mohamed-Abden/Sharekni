6f0af409c1a72dacd3d20ddf510965997648b148
Mohamed Abd El-latef <mohamed.abden2012@gmail.com>
2015-12-19 03:28:28 +0200

    ...

--- Sharekni/Classes/Components/MJPopupBackgroundView/VerifyMobileViewController.m
+++ Sharekni/Classes/Components/MJPopupBackgroundView/VerifyMobileViewController.m
@@ -14,7 +14,7 @@
 #import "HelpManager.h"
 
 @interface VerifyMobileViewController ()
-
+@property (nonatomic,strong) UIToolbar *inputAccessoryView;
 @end
 
 @implementation VerifyMobileViewController
@@ -34,6 +34,8 @@
         self.headerTitle2.textAlignment = NSTextAlignmentRight ;
     }
     
+    [verificationCodeTxt setInputAccessoryView:self.inputAccessoryView];
+    
     verificationCodeTxt.placeholder = GET_STRING(@"Enter verification code");
     [self.headerTitle setText:GET_STRING(@"Write Your Code")];
     [self.headerTitle2 setText:GET_STRING(@"Your Code")];
@@ -41,13 +43,11 @@
     [self.resendBtn setTitle:GET_STRING(@"Resend verification code") forState:UIControlStateNormal];
 }
 
-- (void)HideKeyboard
-{
+- (void)HideKeyboard{
     [verificationCodeTxt resignFirstResponder];
 }
 
-- (IBAction)closePopup:(id)sender
-{
+- (IBAction)closePopup:(id)sender{
     if (verificationCodeTxt.text.length == 0) {
         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:GET_STRING(@"Attention") message:GET_STRING(@"Please write your verification code") delegate:nil cancelButtonTitle:GET_STRING(@"Ok") otherButtonTitles:nil, nil];
         [alert show];
@@ -60,10 +60,9 @@
     {
         [KVNProgress dismiss];
         
-        [KVNProgress showSuccessWithStatus:GET_STRING(@"Mobile Verified")];
-
         if ([user containsString:@"1"])
         {
+            [KVNProgress showSuccessWithStatus:GET_STRING(@"Mobile Verified")];
             if (self.delegate && [self.delegate respondsToSelector:@selector(dismissButtonClicked:)]) {
                 [self.delegate dismissButtonClicked:self];
             }
@@ -76,8 +75,7 @@
     }];
 }
 
-- (IBAction)resendVerification:(id)sender
-{
+- (IBAction)resendVerification:(id)sender{
     [[MobAccountManager sharedMobAccountManager] verifyMobileNumber:self.accountID WithSuccess:^(NSString *user)
      {
          if ([user isEqualToString:@"1"]) {
@@ -96,6 +94,22 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (UIToolbar *)inputAccessoryView{
+    if (!_inputAccessoryView) {
+        _inputAccessoryView = [[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 50)];
+        _inputAccessoryView.barStyle = UIBarStyleDefault;
+        _inputAccessoryView.items = [NSArray arrayWithObjects:
+                                     [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(doneTapped)],
+                                     nil];
+        [_inputAccessoryView sizeToFit];
+    }
+    return _inputAccessoryView;
+}
+
+- (void) doneTapped{
+    [self.view endEditing:YES];
 }
 
 /*
